fe28aa474ab776fccdbb589cda94d58a57047346
jacob <jacob.situ@ucalgary.ca>
2026-03-22 15:35:19 -0600

    fix/added sample tests for pipeline to run

---
+++ backend/api/src/health/health.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { App } from 'supertest/types';
+import request from 'supertest';
+import { HealthController } from './health.controller';
+
+describe('HealthController (unit)', () => {
+  let app: INestApplication<App>;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('health check should return status ok (200)', async () => {
+        await request(app.getHttpServer())
+        .get('/health')
+        .expect({ status: 'ok' });
+    });
+});
