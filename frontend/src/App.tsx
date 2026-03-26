ed8bf13ad01ead7d3f2df621de621f80c6bddafd
Chantal del Carmen <chantal.delcarmen@ucalgary.ca>
2026-03-26 01:23:53 -0600

    feat: add /profile and /settings routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import { CreateExperience } from "./pages/CreateExperience";
 import { SignIn } from "./pages/SignIn";
 import Upload from "./pages/Upload";
+import { Profile } from "./pages/Profile";
+import { Settings } from "./pages/Settings";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path="/create-experience" element={<CreateExperience />}/>
         <Route path="/signin" element={<SignIn />}/>
         <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
