391f820eb9526106ca67acfe97a1d71ca7dfb35c
jessicathomas13 <jessica.thomas1@ucalgary.ca>
2026-03-26 08:45:41 -0600

    fix: resolve app.module.ts  merge conflict

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -5,11 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
 import { SupabaseModule } from './supabase/supabase.module';
 import { AuthModule } from './auth/auth.module';
-<<<<<<< HEAD
 import { FragmentsModule } from './fragments/fragments.module';
-=======
 import { ExperiencesModule } from './experiences/experiences.module';
->>>>>>> 680a8f5 (feat: add Experience CRUD API endpoints)
 
 @Module({
   imports: [
@@ -18,11 +15,8 @@ import { ExperiencesModule } from './experiences/experiences.module';
     }),
     SupabaseModule,
     AuthModule,
-<<<<<<< HEAD
     FragmentsModule,
-=======
     ExperiencesModule,
->>>>>>> 680a8f5 (feat: add Experience CRUD API endpoints)
   ],
   controllers: [AppController, HealthController],
   providers: [AppService],
