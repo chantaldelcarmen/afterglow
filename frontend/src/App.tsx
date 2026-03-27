75ad0a3ee887eb2008d9b7f2fb80ed2d45dba27b
Chantal del Carmen <chantal.delcarmen@ucalgary.ca>
2026-03-27 12:39:46 -0600

    Revert "revert: undo route guards -- will go through MR"
    This reverts commit 4befb53fc7687571e06d1baee0edf59ce582988b.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,28 +13,36 @@ import { Library } from "./pages/Library";
 import { Insights } from "./pages/Insights";
 import { AdminDashboard } from "./pages/AdminDashboard";
 import { PlatformReviewer } from "./pages/PlatformReviewer";
+import { Unauthorized } from "./pages/Unauthorized";
 import { AmbientBackground } from "./components/AmbientBackground";
 import { BottomNav } from "./components/BottomNav";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AmbientBackground>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/insights" element={<Insights />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/create-experience" element={<CreateExperience />} />
-          <Route path="/experience/:id/edit" element={<EditExperience />} />
-          <Route path="/upload" element={<Upload />} />
+          {/* Public routes */}
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/reviewer" element={<PlatformReviewer />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+
+          {/* Authenticated routes */}
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/library" element={<ProtectedRoute><Library /></ProtectedRoute>} />
+          <Route path="/insights" element={<ProtectedRoute><Insights /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+          <Route path="/create-experience" element={<ProtectedRoute><CreateExperience /></ProtectedRoute>} />
+          <Route path="/experience/:id/edit" element={<ProtectedRoute><EditExperience /></ProtectedRoute>} />
+          <Route path="/upload" element={<ProtectedRoute><Upload /></ProtectedRoute>} />
+
+          {/* Role-restricted routes */}
+          <Route path="/reviewer" element={<ProtectedRoute allowedRoles={["platform_reviewer", "admin"]}><PlatformReviewer /></ProtectedRoute>} />
+          <Route path="/admin" element={<ProtectedRoute allowedRoles={["admin"]}><AdminDashboard /></ProtectedRoute>} />
         </Routes>
         <BottomNav />
       </AmbientBackground>
